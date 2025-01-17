6f6b6f40d87a056f6189b6802af64c84b60ca879
Matheus Tito <mthsilva0@gmail.com>
2025-01-16 21:13:47 -0300

    Fix first input

--- caesar.c
+++ caesar.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <string.h>
 
 int main (void) {
     int key = 0;
 
     do {
-        printf("Key:\n");
-        scanf("%d",key);
+        printf("Key: ");
+        scanf("%d", &key);
     } while (key < 0);
-
-    printf("%d", key);
 }
