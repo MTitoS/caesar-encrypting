0e955d992c068f9565f19a35bab8de35c7363361
Matheus Tito <mthsilva0@gmail.com>
2025-01-22 14:20:37 -0300

    Finished problem set

--- caesar.c
+++ caesar.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <string.h>
+#include <ctype.h>
 
-int encodeWord (int key, char text[30]);
+int encodeWord (int originalKey, char text[30]);
 
 int main (void) {
     int key = 0;
@@ -14,18 +15,27 @@ int main (void) {
     } while (key < 0);
 
     printf("Plain text: ");
-    scanf("%29s", plainText);
+    scanf( " %99[^\n]", plainText);
 
     encodeWord(key, plainText);
 }
 
-int encodeWord (int key, char text[30]) {
-    printf("Plain text: %d\nKey: %d", text[0], key);
+int encodeWord (int originalKey, char text[30]) {
+    int newKey = originalKey % 26;
+    char encodedText[30];
 
-    int temp = text - "0";
-    temp += key;
+    printf("Encoded text: ");
 
-    //Preciso transformar em string novamente
-    
-    //printf("%s", temp[0]);
+    for (int i = 0; i < strlen(text); i++) {
+        if (isspace(text[i])) {
+            printf(" ");
+        } else if (isalpha(text[i])) {
+            char base = isupper(text[i]) ? 'A' : 'a';
+            encodedText[i] = (text[i] - base + newKey) % 26 + base;
+        } else {
+                encodedText[i] = text[i];
+        }
+
+        printf("%c", encodedText[i]);
+    }
 }
