ccaa42fd65e5f02392b1688150c7763dfe193ba3
Matheus Tito <mthsilva0@gmail.com>
2025-01-21 21:11:37 -0300

    Added start of validation

--- caesar.c
+++ caesar.c
@@ -2,11 +2,30 @@
 #include <string.h>
 #include <string.h>
 
+int encodeWord (int key, char text[30]);
+
 int main (void) {
     int key = 0;
+    char plainText[30];
 
     do {
         printf("Key: ");
         scanf("%d", &key);
     } while (key < 0);
+
+    printf("Plain text: ");
+    scanf("%29s", plainText);
+
+    encodeWord(key, plainText);
+}
+
+int encodeWord (int key, char text[30]) {
+    printf("Plain text: %d\nKey: %d", text[0], key);
+
+    int temp = text - "0";
+    temp += key;
+
+    //Preciso transformar em string novamente
+    
+    //printf("%s", temp[0]);
 }
